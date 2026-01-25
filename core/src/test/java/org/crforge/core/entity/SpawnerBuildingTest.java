6da591e69bb782004a788177caae46e60c0fb97a
voon <voonhousu@gmail.com>
2026-01-26 01:52:14 +0800

    Fix playable building health not decaying

--- core/src/test/java/org/crforge/core/entity/SpawnerBuildingTest.java
+++ core/src/test/java/org/crforge/core/entity/SpawnerBuildingTest.java
@@ -20,22 +20,25 @@ class SpawnerBuildingTest {
     AbstractEntity.resetIdCounter();
     spawnedEntities = new ArrayList<>();
 
-    skeletonStats = TroopStats.builder()
-        .name("Skeleton")
-        .health(67)
-        .damage(67)
-        .speed(1.2f)
-        .build();
+    skeletonStats =
+        TroopStats.builder()
+            .name("Skeleton")
+            .health(67)
+            .damage(67)
+            .speed(1.2f)
+            .build();
 
-    spawner = SpawnerBuilding.builder()
-        .name("Tombstone")
-        .team(Team.BLUE)
-        .position(10, 10)
-        .spawnInterval(3.0f)
-        .deathSpawnCount(4)
-        .spawnStats(skeletonStats)
-        .lifetime(40f)
-        .build();
+    spawner =
+        SpawnerBuilding.builder()
+            .name("Tombstone")
+            .team(Team.BLUE)
+            .position(10, 10)
+            .spawnInterval(3.0f)
+            .deathSpawnCount(4)
+            .spawnStats(skeletonStats)
+            .lifetime(40f)
+            .maxHealth(100) // Small health to test decay easily
+            .build();
 
     spawner.setSpawnCallback(spawnedEntities::add);
     spawner.onSpawn();
@@ -78,5 +81,17 @@ class SpawnerBuildingTest {
 
     // Check that they are skeletons
     assertThat(spawnedEntities).allMatch(e -> e.getName().equals("Skeleton"));
+  }
+
+  @Test
+  void health_shouldDecayOverTime() {
+    // Lifetime 40s, Health 100
+    // Decay rate = 100 / 40 = 2.5 HP/s
+    int initialHealth = spawner.getHealth().getCurrent();
+
+    // Run for 4 seconds -> should lose 10 HP
+    spawner.update(4.0f);
+
+    assertThat(spawner.getHealth().getCurrent()).isEqualTo(initialHealth - 10);
   }
 }
