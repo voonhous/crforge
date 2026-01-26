0c68832e18643b44a902ad246f7cf634be5a50cf
voon <voonhousu@gmail.com>
2026-01-26 18:32:52 +0800

    Add card loader to separate data from compute logic

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,6 +6,12 @@ dependencies {
     // JSON parsing for card data
     api("com.fasterxml.jackson.core:jackson-databind:${property("jacksonVersion")}")
 
+    // Logging
+    api("org.slf4j:slf4j-api:2.0.12")
+
+    // Use simple logger for now in tests/CLI
+    testImplementation("org.slf4j:slf4j-simple:2.0.12")
+
     // Lombok
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
